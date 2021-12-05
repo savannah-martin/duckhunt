28f2c5f00af7cae67b82fbefc753f5b1d6210a1b
savannah-martin <78399457+savannah-martin@users.noreply.github.com>
2021-12-04 20:56:43 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,111 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="top">
+    <h2>Total:{{ total }}</h2>
+    <h2>Time Remaining:{{ timeRemaining }}s</h2>
+  </div>
+
+  <div class="sky">
+    <Duck v-if="isPlaying" :delay="1000" @hit="handleHit" />
+    <Duck v-if="isPlaying" :delay="1500" @hit="handleHit" />
+    <Duck v-if="isPlaying" :delay="2500" @hit="handleHit" />
+    <Duck v-if="isPlaying" :delay="2000" @hit="handleHit" />
+    <h1 class="score">{{score}}</h1>
+  </div>
+
+  <div class="ground">
+    <div>
+      <button class="btn" @click="startGame">Start</button>
+    </div>
+    <div>
+      <button class="btn stop" @click="stopGame">Stop</button>
+    </div>
+  </div>
+
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Duck from "./components/Duck.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld
+    Duck,
+  },
+  data() {
+    return {
+      score: 0,
+      total: 0,
+      timeRemaining: 0,
+      isPlaying: false,
+      delay: 1000,
+    };
+  },
+  methods: {
+    startGame() {
+      const interval = setInterval(() => {
+        this.timeRemaining -= 1;
+        if (this.timeReinaing === 0) {
+          this.stopGame();
+          clearInterval(interval);
+        }
+      }, 1000);
+      this.timeRemaining = 10;
+      this.isPlaying = true;
+    },
+    stopGame() {
+      this.isPlaying = false;
+    },
+    handleHit(responseTime) {
+      this.score = (this.delay - responseTime) * 4;
+      this.total += this.score;
+    },
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+
+.top {
+  height: 10vh;
+}
+
+.sky {
+  background: lightskyblue;
+  height: 70vh;
+  position: relative;
+}
+
+.ground {
+  height: 20vh;
+  background: rgb(81, 187, 81);
+  display: flex;
+  justify-content: space-evenly;
+}
+button {
+  padding: 20px;
+  font-size: 20px;
+  width: 400px;
+  background-color: rgb(25, 77, 38);
+  margin-top: 15px;
+}
+.stop {
+  background-color: rgb(148, 55, 55);
 }
 </style>
---
+++ src/components/Duck.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="duck" :style="{ top: top + '%', left: left + '%' }" @click="hit">
+    <img src="../assets/duck.png" alt="duck" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Duck",
+  props: {},
+  data() {
+    return {
+      top: 4,
+      left: 40,
+      isVisible: false,
+      startTime: null,
+    };
+  },
+  methods: {
+    hit() {
+      this.isVisible = false;
+      const endTime = Date.now();
+      const responseTime = this.startTime - endTime;
+      this.$emit("hit", responseTime);
+    },
+    showDuck() {
+      this.top = Math.random() * 85 + 5;
+      this.left = Math.random() * 90 + 5;
+      this.isVisible = true;
+      this.startTime = Date.now();
+    },
+  },
+  mounted() {
+    this.showDuck();
+    setInterval(() => {
+      this.show.Duck();
+    }, this.delay);
+  },
+};
+</script>
+
+<style scoped>
+.duck {
+  position: absolute;
+  width: 80px;
+  left: 70%;
+  top: 50%;
+  cursor: crosshair;
+}
+
+img {
+  width: 100%;
+}
+</style>
